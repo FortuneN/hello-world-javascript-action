e7b7a04242f5e5086cf2b14253e5b6c6582b04be
Fortune Ngwenya <fortune.ngwenya@platform45.com>
2023-03-13 12:52:37 +0200

    My first action is ready

--- push.ps1
+++ push.ps1
@@ -1,4 +1,6 @@
+git tag -d v1
+git push --tags
 git add .
 git commit -m "My first action is ready"
-git tag -a -f -m "My first action release" v1.1
+git tag -a -f -m "My first action release" v1
 git push --follow-tags
